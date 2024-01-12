920deab68d280de1412003ca43b4df9f932064c3
mermer vivo <mincoln419@naver.com>
2024-01-13 07:43:40 +0900

    refs #2 dummy data and image

---
+++ toss-app/lib/screen/main/tab/home/banks_dummy.dart
@@ -0,0 +1,8 @@
+import 'package:fast_app_base/screen/main/tab/home/vo/vo_bank.dart';
+
+import '../../../../common/constants.dart';
+
+final bankInvest = Bank(name: "투자신탁", logoImagePath: "$basePath/bank/bank_investment.png");
+final bankKakao = Bank(name: "카카오뱅크", logoImagePath: "$basePath/bank/bank_kakao.png");
+final bankShinhan = Bank(name: "신한은행", logoImagePath: "$basePath/bank/bank_shinhan.png");
+final bankToss = Bank(name: "토스뱅크", logoImagePath: "$basePath/bank/bank_toss.png");
