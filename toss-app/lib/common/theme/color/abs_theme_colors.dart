4107c8dda393106acd7815716cd4b046341e41d5
mingeon cho <mincoln419@naver.com>
2024-01-17 15:05:18 +0900

    refs #2 주식 탭 my account

--- toss-app/lib/common/theme/color/abs_theme_colors.dart
+++ toss-app/lib/common/theme/color/abs_theme_colors.dart
@@ -59,4 +59,6 @@ abstract class AbstractThemeColors {
   Color get lessImportantComponent => AppColors.grey;
 
   Color get blueText => AppColors.blue;
+
+  Color get plus => const Color.fromARGB(255, 230, 71, 83);
 }
