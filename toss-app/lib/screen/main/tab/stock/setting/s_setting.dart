93d9b93698c2d545f65f9ded43e23dfbcb55df7a
mermer vivo <mincoln419@naver.com>
2024-01-20 15:45:31 +0900

    refs #2 토스앱 MVP 화면 마무리 - 오픈소스 화면

--- toss-app/lib/screen/main/tab/stock/setting/s_setting.dart
+++ toss-app/lib/screen/main/tab/stock/setting/s_setting.dart
@@ -5,6 +5,7 @@ import 'package:fast_app_base/common/data/preference/prefs.dart';
 import 'package:fast_app_base/common/widget/w_big_button.dart';
 import 'package:fast_app_base/screen/main/tab/stock/setting/d_number.dart';
 import 'package:fast_app_base/screen/main/tab/stock/setting/w_switch_menu.dart';
+import 'package:fast_app_base/screen/opensource/s_opensource.dart';
 import 'package:flutter/material.dart';
 import 'package:quiver/testing/src/time/time.dart';
 
@@ -61,6 +62,9 @@ class _SettingScreenState extends State<SettingScreen> {
                     }
                   }),
             ),
+            BigButton('오픈소스화면', onTap: () async {
+              Nav.push(OpensourceScreen());
+            }),
           ],
         ));
   }
