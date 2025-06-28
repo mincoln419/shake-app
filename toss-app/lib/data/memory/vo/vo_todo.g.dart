5d3cebd6069eef6f7fb62dead5c91545fd7b40f7
mermer-mac <mincoln419@naver.com>
2025-06-28 23:40:32 +0900

    commig

--- toss-app/lib/data/memory/vo/vo_todo.g.dart
+++ toss-app/lib/data/memory/vo/vo_todo.g.dart
@@ -9,22 +9,18 @@ part of 'vo_todo.dart';
 Todo _$TodoFromJson(Map<String, dynamic> json) => Todo(
       id: json['id'] as int,
       title: json['title'] as String,
-      dueDtm: DateTime.parse(json['dueDate'] as String),
+      dueDtm: DateTime.parse(json['dueDtm'] as String),
       modifyTime: json['modifyTime'] == null
           ? null
           : DateTime.parse(json['modifyTime'] as String),
       status: $enumDecodeNullable(_$TodoStatusEnumMap, json['status']),
-      createdDtm: json['createdTime'] == null
-          ? null
-          : DateTime.parse(json['createdTime'] as String),
     );
 
 Map<String, dynamic> _$TodoToJson(Todo instance) => <String, dynamic>{
       'id': instance.id,
       'title': instance.title,
-      'createdTime': instance.createdTime.toIso8601String(),
       'modifyTime': instance.modifyTime?.toIso8601String(),
-      'dueDate': instance.dueDtm.toIso8601String(),
+      'dueDtm': instance.dueDtm.toIso8601String(),
       'status': _$TodoStatusEnumMap[instance.status]!,
     };
 
